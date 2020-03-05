ac8d00697d7fc68c540cba0defe538fdb94f7e41
Marina <59802938+squirrell50@users.noreply.github.com>
2020-03-05 10:11:26 +0300

    Add files via upload

--- src/App1.js
+++ src/App1.js
@@ -5,12 +5,13 @@ import App2 from "./App2";
 class App1 extends React.Component{
    constructor(props){
        super(props);
-        const result1 = this.props.num*2;
-   };
-   render() {
+       //this.result1 = this.props.num*2;
+     };
 
+   render() {
+    const   result1 = this.props.num*2;
       return (
-          <App2 result={this.result1}/>
+          <App2 result={result1}/> //this.result1
           )
      };
 }
